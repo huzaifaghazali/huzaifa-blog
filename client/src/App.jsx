c054ea10737805e9d9c67d1d279f2f0c5a768dbc
huzaifaghazali <huzaifaghazali@gmail.com>
2024-05-28 19:01:40 +0500

    Create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter className='text-3xl'>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
