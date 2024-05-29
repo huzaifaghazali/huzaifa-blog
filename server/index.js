2f9f810f1e132fee2aebbe1dab10b066da21bf19
huzaifaghazali <huzaifaghazali@gmail.com>
2024-05-29 20:26:50 +0500

    Create a test API route

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import { mongoConnect } from './DB/mongo.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/api/user', userRoutes);
 
 async function startServer() {
   await mongoConnect();
