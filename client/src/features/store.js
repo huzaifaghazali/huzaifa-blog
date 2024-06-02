b6a0d57a36fd2f6fdb9b1ef31e26503d0c6d5725
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-02 16:05:16 +0500

    Add redux persist

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/userSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+// Combine all the reducers into a single root reducer
+const rootReducer = combineReducers({
+  // This is the user reducer that handles all the user-related state
+  user: userReducer,
 });
+
+// Configuration for redux-persist
+const persistConfig = {
+  // The key under which the persisted state will be stored in the storage
+  key: 'root',
+  // The storage engine used to persist the state
+  storage,
+  // The version of the persisted state
+  version: 1,
+};
+
+// Create a reducer that will persist the state
+const persistReducers = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  // Set the root reducer that will handle all the state
+  reducer: persistReducers,
+  // Configure the middleware
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // Disable the serializableCheck middleware for now
+      // This middleware checks if the actions are serializable (can be safely converted to JSON)
+      // Since we're using a custom middleware for handling Firebase actions, we can disable this check
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
-import { store } from './features/store.js';
+import { store, persistor } from './features/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
