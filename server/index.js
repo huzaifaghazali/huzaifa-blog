01e62a02c66fa784b3c0b45748bac14f6b52c6c5
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-06 13:52:23 +0500

    Add create a post API route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import { mongoConnect } from './DB/mongo.js';
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import postRoutes from './routes/postRoute.js';
 
 dotenv.config();
 const app = express();
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 async function startServer() {
   await mongoConnect();
