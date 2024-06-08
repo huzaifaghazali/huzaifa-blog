6412eec5722aaec86c7cdfe1c078d10de01c8c5b
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-08 16:09:46 +0500

    Create get users API route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -83,3 +83,62 @@ export const signout = (req, res, next) => {
     next(error);
   }
 };
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to see all users'));
+  }
+
+  // Extract the query parameters from the request
+  const { startIndex, limit, order } = req.query;
+
+  try {
+    // Parse the startIndex parameter into an integer, defaulting to 0 if it is not a valid number.
+    const startingIndex = parseInt(startIndex) || 0;
+    // Parse the limit parameter into an integer, defaulting to 9 if it is not a valid number.
+    const maxLimit = parseInt(limit) || 9;
+    // Determine the sort direction based on the order parameter, defaulting to descending order.
+    const sortDirection = order === 'asc' ? 1 : -1;
+
+    // Retrieve the users from the database, sorted by creation date in the specified direction,
+    // skipping the specified number of users and limiting the number of users retrieved.
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startingIndex)
+      .limit(maxLimit);
+
+    // Remove the password property from each user object before returning it.
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    // Count the total number of users in the database.
+    const totalUsers = await User.countDocuments();
+
+    // Get the current date.
+    const now = new Date();
+
+    // Calculate the date one month ago.
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    // Count the number of users created in the previous month.
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    // Return the JSON object containing the users, total users, and last month's users.
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    // If an error occurs during the retrieval process, call the next middleware function with the error.
+    next(error);
+  }
+};
