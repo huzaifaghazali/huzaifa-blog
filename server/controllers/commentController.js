9fbf2cfe8ce1c654d4351b3bf14d4a2ba0ffb45d
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-09 18:42:47 +0500

    Add like functionality to the comment component

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -34,3 +34,42 @@ export const getPostsComments = async (req, res, next) => {
     next(error);
   }
 };
+
+export const likeComment = async (req, res, next) => {
+  try {
+    // Find the comment in the database using its id.
+    const comment = await Comment.findById(req.params.commentId);
+
+    // If the comment doesn't exist, return a '404' status code.
+    if (!comment) {
+      return next(errorHandler(404, 'Comment not found'));
+    }
+
+    // Get the index of the current user's id in the comment's likes array.
+    const userIndex = comment.likes.indexOf(req.user.id);
+
+    // If the user is not in the comment's likes array,
+    // it means they haven't liked it yet.
+    if (userIndex === -1) {
+      // Increment the comment's number of likes.
+      comment.numberOfLikes += 1;
+
+      // Add the current user's id to the comment's likes array.
+      comment.likes.push(req.user.id);
+    } else {
+      // If the user is in the comment's likes array,
+      // it means they have already liked it.
+
+      // Decrement the comment's number of likes.
+      comment.numberOfLikes -= 1;
+
+      // Remove the current user's id from the comment's likes array.
+      comment.likes.splice(userIndex, 1);
+    }
+
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, getPostsComments } from '../controllers/commentController.js';
+import {
+  create,
+  getPostsComments,
+  likeComment,
+} from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.get('/getPostComments/:postId', getPostsComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
