9344f298f4b37934676e4d062771efb26e606a6b
huzaifaghazali <huzaifaghazali@gmail.com>
2024-06-10 12:49:12 +0500

    Add delete functionality to the comment

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-function Comment({ comment, onLike, onEdit }) {
+function Comment({ comment, onLike, onEdit, onDelete }) {
   const { currentUser } = useSelector((state) => state.user);
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
@@ -125,6 +125,13 @@ function Comment({ comment, onLike, onEdit }) {
                       className='text-gray-400 hover:text-blue-500'
                     >
                       Edit
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-400 hover:text-red-500'
+                    >
+                      Delete
                     </button>
                   </>
                 )}
--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
   create,
+  deleteComment,
   editComment,
   getPostsComments,
   likeComment,
@@ -13,5 +14,6 @@ router.post('/create', verifyToken, create);
 router.get('/getPostComments/:postId', getPostsComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
